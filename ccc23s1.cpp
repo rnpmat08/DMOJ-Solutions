174b90b3d0ff92b9b9c7833593ac1ff71046ad47
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:08:20 -0400

    Simplified ccc23s1.cpp

--- ccc23s1.cpp
+++ ccc23s1.cpp
@@ -1,38 +1,23 @@
-#pragma GCC optimize("Ofast,unroll-loops")
 #include <iostream>
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
 
 using namespace std;
 int F[2][200000];
 
 int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
     int N, A = 0;
-    scan(N);
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < N; j++) {
-            scan(F[i][j]);
-        }
-    }
+    cin >> N;
+    for (int i = 0; i < 2; i++) for (int j = 0; j < N; j++) cin >> F[i][j];
     for (int i = 0; i < N; i++) {
         if (F[0][i] == 1) {
             A += 3;
-            if (i > 0 && F[0][i - 1] == 1) {
-                A -= 2;
-            }
+            if (i > 0 && F[0][i - 1] == 1) A -= 2;
         }
         if (F[1][i] == 1) {
             A += 3;
-            if (i > 0 && F[1][i - 1] == 1) {
-                A -= 2;
-            }
-            if (i % 2 == 0 && F[0][i] == 1) {
-                A -= 2;
-            }
+            if (i > 0 && F[1][i - 1] == 1) A -= 2;
+            if (i % 2 == 0 && F[0][i] == 1)A -= 2;
         }
     }
-    printf("%d", A);
+    cout << A;
     return 0;
 }
