daadef41fb1175d944b689a5d1eb75c57122837c
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:17:24 -0400

    Simplified ccc23s1.cpp again

--- ccc23s1.cpp
+++ ccc23s1.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int F[2][200000];
 
 int main() {
-    int N, A = 0;
-    cin >> N;
+    int N, A = 0; cin >> N;
     for (int i = 0; i < 2; i++) for (int j = 0; j < N; j++) cin >> F[i][j];
     for (int i = 0; i < N; i++) {
         if (F[0][i] == 1) {
