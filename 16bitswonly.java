76de3d8b121ec0ca33515d90d1cbf59008f33d86
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:20:57 -0400

    Add files via upload

---
+++ 16bitswonly.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int N = readInt(); long A, B, P;
+		for (int i = 0; i < N; i++) {
+			A = readLong(); B = readLong(); P = readLong();
+			if (A * B != P) {
+    				System.out.println("16 BIT S/W ONLY");
+			} else {
+    				System.out.println("POSSIBLE DOUBLE SIGMA");
+			}
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+}
