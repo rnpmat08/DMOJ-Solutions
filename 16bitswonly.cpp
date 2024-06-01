1b6ffd73923d5bffe3c4e63c1412b9aac936225f
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:06:30 -0400

    Simplified 16bitswonly.cpp

--- 16bitswonly.cpp
+++ 16bitswonly.cpp
@@ -1,19 +1,13 @@
-#pragma GCC optimize("Ofast,unroll-loops")
 #include <iostream>
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N;
-    long long A, B, P;
-    scan(N);
+    int N; long long A, B, P;
+    cin >> N;
     for (int i = 0; i < N; i++) {
-        scan(A); scan(B); scan(P);
-        (A * B != P) ? printf("16 BIT S/W ONLY\n") : printf("POSSIBLE DOUBLE SIGMA\n");
+        cin >> A >> B >> P;
+        (A * B != P) ? cout << "16 BIT S/W ONLY\n" : cout << "POSSIBLE DOUBLE SIGMA\n";
     }
     return 0;
 }
