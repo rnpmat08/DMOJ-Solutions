76e16278382a2f82f11286b670feeae24d58a0ff
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:15:03 -0400

    Simplified yac6p1.cpp

--- yac6p1.cpp
+++ yac6p1.cpp
@@ -1,18 +1,13 @@
-#pragma GCC optimize("Ofast,unroll-loops")
 #include <iostream>
-#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
-char _;
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N, M; scan(N); scan(M);
+    int N, M; cin >> N >> M; 
     for (int i = 1; i <= N; i++) {
         for (int j = i + 1; j <= N; j++) {
             if (M == 0) return 0;
-            printf("%d ", i); printf("%d\n", j);
+            cout << i << " " << j << "\n";
             M--;
         }
     }
