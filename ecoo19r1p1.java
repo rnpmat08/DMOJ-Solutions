f43eb56da28abf8391c0236fde0f855c77d44976
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2024-06-01 18:04:30 -0400

    Add files via upload

---
+++ ecoo19r1p1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		for (int a = 0; a < 10; a++) {
+			int N = readInt(), M = readInt(), D = readInt(), CD = 1, L = 0, TC = N;
+			int A[] = new int[D + 1];
+			Arrays.fill(A, 0);
+			for (int i = 0; i < M; i++) {
+				A[readInt()]++;
+			}
+			for (int i = 1; i <= D; i++) {
+				if (N == 0) {
+					L++;
+					N = TC;
+				}
+				N--;
+				TC += A[i];
+                		N += A[i];
+			}
+			System.out.println(L);
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+}
