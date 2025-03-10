d7ddee58ef593c32985ca2ae841fa1fba029ca8e
rnpmat08 <156210000+rnpmat08@users.noreply.github.com>
2025-03-10 13:09:22 -0400

    Found a simpler solution

--- ccc11j5.cpp
+++ ccc11j5.cpp
@@ -3,18 +3,12 @@
 
 using namespace std;
 
-vector<int> g[7];
-
-int dfs(int i) {
-    if (g[i].empty()) return 1;
-    int ans = 1;
-    for (int u : g[i]) ans *= dfs(u) + 1;
-    return ans;
-}
+int w[7];
 
 int main() {
     int n; cin >> n;
-    for (int i = 1, j; i <= n - 1; i++) { cin >> j; g[j].push_back(i); }
-    cout << dfs(n);
+    for (int i = 1; i <= n; i++) w[i] = 1;
+    for (int i = 1, j; i <= n - 1; i++) { cin >> j; w[j] *= w[i] + 1; }
+    cout << w[n];
     return 0;
 }
